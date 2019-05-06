db95b4905b36081d29bd9c6fd12fa8939bb04df1
Fabiane Albuquerque <bii.nett@gmail.com>
2019-05-06 15:50:46 -0300

    correcao e atualizacao do arquivo do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,51 @@ module.exports = {
         'semi': [
             'error',
             'always'
+        ],
+        'camelcase': 2,
+        'id-length': 2,
+        'keyword-spacing': 2,
+        'space-infix-ops': 2,
+        'comma-spacing': 2,
+        'space-before-blocks': 2,
+        'spaced-comment': 2,
+        'key-spacing': 2,
+        'no-multi-spaces': 2,
+        'space-in-parens': [
+            'error',
+            'never'
+        ],
+        'space-before-function-paren': [
+            'error',
+            'never'
+        ],
+        'func-call-spacing': [
+            'error',
+            { 'max': 1 }
+        ],
+        'no-multiple-empty-lines': [
+            'error',
+            { 'max': 1 }
+        ],
+        'padded-blocks': [
+            'error',
+            'never'
+        ],
+        'no-floating-decimal': [ 'error' ],
+        'object-property-newline': [ 'error' ],
+        'brace-style': [ 
+            'error' , 
+            '1tbs' 
+        ],
+        'eqeqeq': [
+            'error', 
+            'always', 
+            { 'null': 'ignore' }
+        ],
+        'no-array-constructor': [ 'error' ],
+        'no-undef': [
+            'error', 
+            { "typeof": true }
         ]
     }
 };
